a36ed5a5d2d15a3b6542eec4c70a7d445cb77742
supersilent <shrikethrush@gmail.com>
2018-11-30 17:34:20 -0800

    pass all tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,8 +11,9 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var dataObj = { results: [] };
 
-var requestHandler = function(request, response) {
+exports.requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -27,10 +28,9 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
+  console.log(
+    "Serving request type " + request.method + " for url " + request.url
+  );
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
@@ -39,20 +39,42 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  headers["Content-Type"] = "application/json";
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  if (request.method === 'GET' && request.url === '/classes/messages') {
+    // The outgoing status.
+    var statusCode = 200;
+    // .writeHead() writes to the request line and headers of the response,
+    // which includes the status and all headers.
+    response.writeHead(statusCode, headers);
+
+    // Make sure to always call response.end() - Node may not send
+    // anything back to the client until you do. The string you pass to
+    // response.end() will be the body of the response - i.e. what shows
+    // up in the browser.
+    //
+    // Calling .end "flushes" the response's internal buffer, forcing
+    // node to actually send all the data over to the client.
+    // console.log(data);
+    // console.log("This is dataObj", dataObj);
+
+    response.end(JSON.stringify(dataObj));
+  } else if (request.method === 'POST' && request.url === '/classes/messages') {
+    let body;
+    request.on('data', chunk => {
+      body = JSON.parse(chunk.toString()); // convert Buffer to string
+      dataObj.results.push(body);
+    });
+
+    var statusCode = 201;
+    response.writeHead(statusCode, headers);
+    response.end();
+  } else {
+    var statusCode = 404;
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,10 +86,9 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
+defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
 };
-
